3176cc3906fc9b1a425628f7ca258385973ea050
satwickvicharapu <148252426+satwickvicharapu@users.noreply.github.com>
2025-10-10 10:25:45 +0530

    Add files via upload

---
+++ patterns code.py
@@ -0,0 +1,33 @@
+# patterns
+n=4
+for i in range (n):
+    for j in range (n):
+        
+        print("*",end='')
+
+
+#this patterns prints side *'s
+n=4
+for i in range (n):
+    for j in range (n):
+        
+        print("*",end='')
+        
+
+#its print the *'s like down 
+
+n=4
+for i in range (n):
+    for j in range (n):
+        
+        print("*",end='')
+        print()
+
+#this pattern prints 4 row's and 4 coloumn's
+n=4
+for i in range (n):
+    for j in range (n):
+        
+        print("*",end='')
+    print()
+    
